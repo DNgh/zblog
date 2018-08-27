54283ea913fc4de1c39f2320b2e5b5d15243a2c8
zhouzm <8115052+DNgh@users.noreply.github.com>
2018-08-28 07:58:56 +0800

    add zblog-facility

---
+++ zblog-facility/src/main/java/com/min/zblog/def/UserState.java
@@ -0,0 +1,29 @@
+package com.min.zblog.def;
+
+/**
+ * <p>Title: UserState</p>
+ * <p>Description: 用户状态枚举值</p>
+ * @author	zhouzm
+ * @date	2018年8月26日
+ * @version 1.0.0
+ */
+@EnumInfo({
+	"UNACTIVATED|激活",
+	"NORMAL|正常",
+	"CLOSED|关闭"
+})
+public enum UserState {
+	UNACTIVATED("激活"),
+	NORMAL("正常"),
+	CLOSED("关闭");
+	
+	private String desc;
+	
+	private UserState(String desc){
+		this.desc = desc;
+	}
+	
+	public String getDesc(){
+		return this.desc;
+	}
+}
