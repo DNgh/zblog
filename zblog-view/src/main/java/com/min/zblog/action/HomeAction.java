cfa3b194eabbd017fa4433a1b7c8a20f5aea1e80
zhouzm <zhouzm@allinfinance.com>
2018-08-29 17:50:53 +0800

    add action method

---
+++ zblog-view/src/main/java/com/min/zblog/action/HomeAction.java
@@ -0,0 +1,23 @@
+package com.min.zblog.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HomeAction extends ActionSupport {
+	private static final long serialVersionUID = 7939126579870842596L;
+ 
+    public String execute() {
+        return SUCCESS;
+    }
+    
+    public String home(){
+    	return SUCCESS;
+    }
+    
+    public String about(){
+    	return SUCCESS;
+    }
+    
+    public String contact(){
+    	return SUCCESS;
+    }
+}
