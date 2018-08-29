4e6d6c4a48a2509acec89343469ad54618efec1b
zhouzm <8115052+DNgh@users.noreply.github.com>
2018-08-29 08:02:23 +0800

    add home about contact pages

--- zblog-view/src/main/webapp/custom/js/zblog.js
+++ zblog-view/src/main/webapp/custom/js/zblog.js
@@ -11,10 +11,25 @@ $(function () {
 	    bootstrapMajorVersion: 3,    
 	    alignment:"center",    
 	    numberOfPages:5,
+	    useBootstrapTooltip: true, 
+	    tooltipTitles: function (type, page, current) {
+            switch (type) {
+            case "first":
+                return "跳转到首页";
+            case "prev":
+                return "跳转到上一页";
+            case "next":
+                return "跳转到下一页";
+            case "last":
+                return "跳转到尾页";
+            case "page":
+                return "跳转到第" + page + "页";
+            }
+        },
 	    shouldShowPage:true,
 	    itemContainerClass: function (type, page, current) {
             return (type=="page" && page === current) ? "active" : "pointer-cursor"
-      },
+        },
 	    itemTexts: function (type, page, current) {        
 	        switch (type) {            
 	        case "first": return "首页";            
@@ -24,7 +39,7 @@ $(function () {
 	        case "page": return page;
 	        }
 	    }
-		}
-		$('.homePaginator').bootstrapPaginator(options);
+	}
+	$('.homePaginator').bootstrapPaginator(options);
 		
 })
