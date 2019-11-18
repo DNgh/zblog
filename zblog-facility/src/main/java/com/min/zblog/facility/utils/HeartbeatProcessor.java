faef594a0a123b593680a1ef1509f5a4374b54b6
zhouzm <8115052+DNgh@users.noreply.github.com>
2019-11-18 18:37:24 +0800

    心跳、程序状态更新

---
+++ zblog-facility/src/main/java/com/min/zblog/facility/utils/HeartbeatProcessor.java
@@ -0,0 +1,65 @@
+package com.min.zblog.facility.utils;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
+import java.lang.management.MemoryUsage;
+import java.text.MessageFormat;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.SmartLifecycle;
+
+/**
+ * #心跳、进程运行状态更新
+ * @author zhouzm
+ *
+ */
+public class HeartbeatProcessor implements SmartLifecycle {
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
+	private boolean runningFlag = false;
+	
+	@Override
+	public void start() {
+		logger.info("启动进程");
+		runningFlag = true;
+	}
+
+	@Override
+	public void stop() {
+		logger.info("关闭进程");
+		runningFlag = false;
+	}
+
+	@Override
+	public boolean isRunning() {
+		return runningFlag;
+	}
+
+	@Override
+	public int getPhase() {
+		//在最后一个初始化，第一个关闭
+		return Integer.MAX_VALUE;
+	}
+
+	@Override
+	public boolean isAutoStartup() {
+		return true;
+	}
+
+	@Override
+	public void stop(Runnable callback) {
+		logger.info("关闭进程");
+		callback.run();
+		runningFlag = false;
+	}
+	
+	public void heartbeat()
+	{
+		MemoryMXBean mmb = ManagementFactory.getMemoryMXBean();
+		MemoryUsage usage = mmb.getHeapMemoryUsage();
+		String memUsage = MessageFormat.format("{0,number,0.00}% ({1}MB)", (1.0 - (double)usage.getUsed() / usage.getMax()) * 100, (usage.getMax() - usage.getUsed()) >> 20);
+		logger.info("心跳数据，剩余内存："+memUsage);
+	}
+
+}
